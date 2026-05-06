8329cc802f9582f7b667f381cff9feeb466a9401
Harshita Swamy <harshitaswamy.connect@gmail.com>
2026-05-06 14:45:30 +0530

    updating

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import app from "./src/app.js";
 import { PORT } from "./src/config/env.config.js";
 import testConnection from "./src/config/db.config.js";  
 
+
 app.listen(PORT, async() => {
     console.log(`Server is running on port http://localhost:${PORT}`);
     await testConnection();              
