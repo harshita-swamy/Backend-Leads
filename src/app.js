d79c3c5401d7da61238d71cb4233c788d557b69d
Harshita Swamy <harshitaswamy.connect@gmail.com>
2026-05-08 20:31:27 +0530

    app.js update

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors()); // 👈 ये line जरूरी है
+app.use(cors()); //  
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
