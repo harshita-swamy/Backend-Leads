86deb25a4ea2ed2978927b760cb8aaf6d3587a4e
Harshita Swamy <harshitaswamy.connect@gmail.com>
2026-05-08 21:03:28 +0530

    remove testConnection

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ import testConnection from "./src/config/db.config.js";
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async() => {
-    console.log(`Server is running on port ${PORT}`);
-    await testConnection();              
+    console.log(`Server is running on port ${PORT}`);    
 });                   
 
   
