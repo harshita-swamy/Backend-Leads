f93ea16041810de6b48b68e6965290e93fe21a8f
Harshita Swamy <harshitaswamy.connect@gmail.com>
2026-05-09 17:34:38 +0530

    changing function

--- server.js
+++ server.js
@@ -4,6 +4,5 @@ import testConnection from "./src/config/db.config.js";
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async() => {
-    console.log(`Server is running on port ${PORT}`);  
-    await testConnection();
+    console.log(`Server is running on port ${PORT}`);
 });
