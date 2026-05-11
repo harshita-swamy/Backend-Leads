49e161620b0ddef1f3566a8b93c7bbcfdc96f4c5
Harshita Swamy <harshitaswamy.connect@gmail.com>
2026-05-11 18:35:50 +0530

    updated config files

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -3,11 +3,11 @@ dotenv.config();
 
 const env = {
   db: {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT) || 3306,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.MYSQLHOST,
+    port: parseInt(process.env.MYSQLPORT) || 3306,
+    database: process.env.MYSQLDATABASE,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
     poolMax: parseInt(process.env.DB_POOL_MAX) || 50,
   },
 
