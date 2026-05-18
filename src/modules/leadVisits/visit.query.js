9bf9b7a1bb34f34356b26fb851381926ba5fad55
Harshita Swamy <harshitaswamy.connect@gmail.com>
2026-05-19 00:59:53 +0530

    correction service and query files

--- src/modules/leadVisits/visit.query.js
+++ src/modules/leadVisits/visit.query.js
@@ -1,7 +1,7 @@
 export const CREATE_VISIT =
 `INSERT INTO lead_visits
-(lead_id, visit_date, notes)
-VALUES (?, ?, ?)`;
+(lead_id, visited_by, visit_date, notes)
+VALUES (?, ?, ?, ?)`;
 
 export const GET_VISITS =
 `SELECT * FROM lead_visits
--- src/modules/leadVisits/visit.service.js
+++ src/modules/leadVisits/visit.service.js
@@ -2,11 +2,17 @@ import { pool } from "../../config/db.config.js";
 import * as query from "./visit.query.js";
 
 export const createVisitService = async (data) => {
-  const { lead_id, visit_date, notes } = data;
+
+  const {
+    lead_id,
+    visited_by,
+    visit_date,
+    notes
+  } = data;
 
   const [result] = await pool.query(
     query.CREATE_VISIT,
-    [lead_id, visit_date, notes]
+    [lead_id, visited_by, visit_date, notes]
   );
 
   return {
@@ -16,6 +22,7 @@ export const createVisitService = async (data) => {
 };
 
 export const getVisitsService = async (lead_id) => {
+
   const [rows] = await pool.query(
     query.GET_VISITS,
     [lead_id]
