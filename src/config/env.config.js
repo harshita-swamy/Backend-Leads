41848c32de81798d92bc3d28117efb452bddbb68
Harshita Swamy <harshitaswamy.connect@gmail.com>
2026-07-03 20:18:07 +0530

    come back to localhost

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -3,18 +3,25 @@ dotenv.config();
 
 const env = {
   db: {
-    host: process.env.MYSQLHOST,
-    port: parseInt(process.env.MYSQLPORT) || 3306,
-    database: process.env.MYSQLDATABASE,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    poolMax: parseInt(process.env.DB_POOL_MAX) || 50,
+    host: process.env.MYSQLHOST || "localhost",
+    port: Number(process.env.MYSQLPORT) || 3306,
+    database: process.env.MYSQLDATABASE || "crm_lms",
+    user: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "",
+    poolMax: Number(process.env.DB_POOL_MAX) || 50,
   },
 
   app: {
-    port: parseInt(process.env.PORT) || 5000,
+    port: Number(process.env.PORT) || 5000,
     env: process.env.NODE_ENV || "development",
   },
 };
 
+
+
+console.log({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  db: process.env.MYSQLDATABASE,
+});
 export default env;
